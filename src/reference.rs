62ab903f7798e865a2248fd6aeaa9cc2d1851a60
Henry de Valence <hdevalence@hdevalence.ca>
2018-07-20 15:24:24 -0700

    Add test of keccakf on zero state

--- src/reference.rs
+++ src/reference.rs
@@ -126,16 +126,52 @@ mod tests {
     use super::*;
 
     #[test]
-    fn dump_rounds_on_zero_input() {
-        let mut st = State([[0u64; 5]; 5]);
+    fn round_on_zero_state() {
+        // https://github.com/gvanas/KeccakCodePackage/blob/master/tests/TestVectors/KeccakF-1600-IntermediateValues.txt
+        //
+        // This is the state obtained by applying the keccakf to the
+        // zero state.
+        let expected = State([
+            [
+                0xF1258F7940E1DDE7,
+                0x84D5CCF933C0478A,
+                0xD598261EA65AA9EE,
+                0xBD1547306F80494D,
+                0x8B284E056253D057,
+            ],
+            [
+                0xFF97A42D7F8E6FD4,
+                0x90FEE5A0A44647C4,
+                0x8C5BDA0CD6192E76,
+                0xAD30A6F71B19059C,
+                0x30935AB7D08FFC64,
+            ],
+            [
+                0xEB5AA93F2317D635,
+                0xA9A6E6260D712103,
+                0x81A57C16DBCF555F,
+                0x43B831CD0347C826,
+                0x01F22F1A11A5569F,
+            ],
+            [
+                0x05E5635A21D9AE61,
+                0x64BEFEF28CC970F2,
+                0x613670957BC46611,
+                0xB87C5A554FD00ECB,
+                0x8C3EE88A1CCF32C8,
+            ],
+            [
+                0x940C7922AE3A2614,
+                0x1841F924A2C509E4,
+                0x16F53526E70465C2,
+                0x75F644E97F30A13B,
+                0xEAF1FF7B5CECA249,
+            ],
+        ]);
 
-        println!("initial state");
-        println!("{:?}", st);
+        let mut zero = State([[0; 5]; 5]);
+        zero.keccakf();
 
-        for i in 0..24 {
-            st.round(ROUND_CONSTANTS[i]);
-            println!("after round {}", i);
-            println!("{:?}", st);
-        }
+        assert_eq!(zero.0, expected.0);
     }
 }
